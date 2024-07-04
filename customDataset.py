17c7294062be7b0166638550da2369a87e136f15
Harshil <harshilgandhi148@gmail.com>
2024-07-04 16:14:23 -0400

    Updated data and dataloader

--- customDataset.py
+++ customDataset.py
@@ -7,14 +7,13 @@ import numpy as np
 import torchvision.transforms as transforms
 import torchvision
 
-class EmotionDataset(Dataset):
+class CKEmotionDataset(Dataset):
     def __init__(self, root_dir, transform, train):
         self.root_dir = root_dir
         self.transform = transform
         self.train = train
 
         self.anger = sorted(os.listdir(os.path.join(self.root_dir, "anger")))
-        self.disgust = sorted(os.listdir(os.path.join(self.root_dir, "disgust")))
         self.fear = sorted(os.listdir(os.path.join(self.root_dir, "fear")))
         self.happiness = sorted(os.listdir(os.path.join(self.root_dir, "happiness")))
         self.neutral = sorted(os.listdir(os.path.join(self.root_dir, "neutral")))
@@ -22,38 +21,35 @@ class EmotionDataset(Dataset):
         self.surprise = sorted(os.listdir(os.path.join(self.root_dir, "surprise")))
 
         self.angerl = len(self.anger)
-        self.disgustl = len(self.disgust)
         self.fearl = len(self.fear)
         self.happinessl = len(self.happiness)
         self.neutrall = len(self.neutral)
         self.sadnessl = len(self.sadness)
         self.surprisel = len(self.surprise)
 
-        self.data = [self.anger, self.disgust, self.fear, self.happiness, self.neutral, self.sadness, self.surprise]
-        self.length = self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall + self.sadnessl + self.surprisel
+        self.data = [self.anger, self.fear, self.happiness, self.neutral, self.sadness, self.surprise]
+        self.length = self.angerl + self.fearl + self.happinessl + self.neutrall + self.sadnessl + self.surprisel
 
 
     def __len__(self):
-        return self.length #845
+        return self.length #786
 
     def sub_folder(self, index):
         if (index < self.angerl):
             return "anger", index
-        elif (index >= self.angerl and index < self.angerl + self.disgustl):
-            return "disgust", index - self.angerl
-        elif (index >= (self.angerl + self.disgustl) and index < (self.angerl + self.disgustl + self.fearl)):
-            return "fear", index - (self.angerl + self.disgustl)
-        elif (index >= (self.angerl + self.disgustl + self.fearl) and index < (self.angerl + self.disgustl + self.fearl + self.happinessl)):
-            return "happiness", index - (self.angerl + self.disgustl + self.fearl)
-        elif (index >= (self.angerl + self.disgustl + self.fearl + self.happinessl) and index < (self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall)):
-            return "neutral", index - (self.angerl + self.disgustl + self.fearl + self.happinessl)
-        elif (index >= (self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall) and index < (self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall + self.sadnessl)):
-            return "sadness", index - (self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall)
+        elif (index >= (self.angerl) and index < (self.angerl + self.fearl)):
+            return "fear", index - (self.angerl)
+        elif (index >= (self.angerl + self.fearl) and index < (self.angerl + self.fearl + self.happinessl)):
+            return "happiness", index - (self.angerl + self.fearl)
+        elif (index >= (self.angerl+ self.fearl + self.happinessl) and index < (self.angerl + self.fearl + self.happinessl + self.neutrall)):
+            return "neutral", index - (self.angerl + self.fearl + self.happinessl)
+        elif (index >= (self.angerl + self.fearl + self.happinessl + self.neutrall) and index < (self.angerl + self.fearl + self.happinessl + self.neutrall + self.sadnessl)):
+            return "sadness", index - (self.angerl + self.fearl + self.happinessl + self.neutrall)
         else:
-            return "surprise", index - (self.angerl + self.disgustl + self.fearl + self.happinessl + self.neutrall + self.sadnessl)
+            return "surprise", index - (self.angerl + self.fearl + self.happinessl + self.neutrall + self.sadnessl)
 
     def __getitem__(self, index):
-        emotions_dict = {"anger": 0, "disgust": 1, "fear": 2, "happiness": 3, "neutral": 4, "sadness": 5, "surprise": 6}
+        emotions_dict = {"anger": 0, "fear": 1, "happiness": 2, "neutral": 3, "sadness": 4, "surprise": 5}
 
         subfolder, mod_index = self.sub_folder(index)
         img_path = os.path.join(self.root_dir, subfolder, self.data[emotions_dict[subfolder]][mod_index])
@@ -70,5 +66,29 @@ class EmotionDataset(Dataset):
 
         return image, target
 
-# e = EmotionDataset("CK", train=False, transform=False)
+class EmotionDataset(Dataset):
+    def __init__(self, root_dir, transform, train):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.train = train
+        self.data = sorted(os.listdir(self.root_dir))
+
+    def __len__(self):
+        return len(os.listdir(self.root_dir)) #28273
+
+    def __getitem__(self, index):
+        path = self.data[index]
+        img_path = os.path.join(self.root_dir, path)
+        image = io.imread(img_path)
+
+        emotions_dict = {"angry": 0, "fear": 1, "happy": 2, "neutral": 3, "sad": 4, "suprise": 5}
+        y_label = torch.tensor(int(emotions_dict.get(path.lower().split('-')[0])))
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, y_label
+
+
+# e = CKEmotionDataset("CK", transform=transforms.ToTensor(), train = False)
 # print(e.__len__())
--- emotionRecognition.py
+++ emotionRecognition.py
@@ -13,6 +13,7 @@ from torch.utils.data import (
     DataLoader,
 )  # Gives easier dataset managment and creates mini batches
 from customDataset import EmotionDataset
+from customDataset import CKEmotionDataset
 
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,45 +26,42 @@ class ConvolutionalNeuralNetwork(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=(2,2), stride =(2,2)) #cuts size in half
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1)) #keeps size
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3,3), stride=(1,1), padding=(1,1)) #keeps size
-        self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))  # keeps size
-        self.fc1 = nn.Linear(64*12*12, num_classes)
+        self.fc1 = nn.Linear(32*12*12, num_classes)
 
     def forward(self, x):
         x = F.relu((self.conv1(x)))
         x = self.pool(x)
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
         x = self.pool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc1(x)
         return x
 
 def save_model(state, filename = "saved_model.pth.tar"):
-    print("Saving Model")
+    print("Saving model")
     torch.save(state, filename)
 
 def load_model(checkpoint):
-    print("Loading Model")
+    print("Loading model")
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
 
 # Hyperparameters
 in_channel = 1
-num_classes = 7
-learning_rate = 0.01
+num_classes = 6
+learning_rate = 0.001
 batch_size = 64
 num_epochs = 11
 
 # Load Data
-data_set = EmotionDataset(root_dir = "CK", transform = transforms.ToTensor(), train=True)
-train_set, test_set = torch.utils.data.random_split(data_set, [775, 70])
+train_set = CKEmotionDataset(root_dir = "CK", transform = transforms.ToTensor(), train=True)
+#test_set = EmotionDataset(root_dir = "Testing", transform = transforms.ToTensor(), train=False)
 train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=0)
-test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+#test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False, num_workers=0)
 
 #init network
 model = ConvolutionalNeuralNetwork().to(device)
-#model = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=False)
 
 #loss - cost function
 criterion = nn.CrossEntropyLoss()
@@ -104,10 +102,10 @@ def check_accuracy(loader, model):
     model.eval()
 
     with torch.no_grad():
-        # if loader.dataset.train:
-        #     print("Checking accuracy on training data")
-        # else:
-        #     print("Checking accuracy on test data")
+        if loader.dataset.train:
+            print("Checking accuracy on training data")
+        else:
+            print("Checking accuracy on test data")
         for x, y in loader:
             x = x.to(device = device)
             y = y.to(device=device)
@@ -132,4 +130,4 @@ if __name__ == "__main__":
 
     #check accuracy
     check_accuracy(train_loader, model)
-    check_accuracy(test_loader, model)
+    #check_accuracy(test_loader, model)
